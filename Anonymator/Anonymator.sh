77a61a6433ffa5352db6a330dfb187db6a81e96f
Z3r0way <timothe.poujol@protonmail.ch>
2018-03-19 19:08:40 +0100

    Bug fixes

--- Anonymator/Anonymator.sh
+++ Anonymator/Anonymator.sh
@@ -2,3 +2,7 @@ sudo macchanger -r $(ip route list | grep default | awk '{print $5} ')
 sudo service network-manager restart
 echo "Mac adress changed"
 sudo torghost start
+clear
+echo "ip adress changed!"
+echo ""
+echo "your ip is:"
--- Anonymator/Install.sh
+++ Anonymator/Install.sh
@@ -4,3 +4,6 @@ sudo apt-get install macchanger
 cd torghost
 chmod +x install.sh
 sudo ./install.sh
+echo "Install finished!"
+echo "Type [enter] to exit"
+read  -n 1 -p "Input Selection:" mainmenuinput
