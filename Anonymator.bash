c8c7c10b5530f54d8a9aedfbf65b91fce1a27b2e
Z3r0way <timothe.poujol@gmail.com>
2018-03-26 13:59:17 +0200

    Adding a network test

--- Anonymator.bash
+++ Anonymator.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Anonymise yourself
 sudo echo ""
+clear
 echo " _____                           _            "
 echo "|  _  |___ ___ ___ _ _ _____ ___| |_ ___ ___  "
 echo "|     |   | . |   | | |     | .'|  _| . |  _| "
@@ -15,10 +16,10 @@ echo "4: Install dependencies (you need to do it once)"
 #Declaring variables
 read -p "Your Choice: " C
 Interface=$(ip route list | grep default | awk '{print $5} ')
-
+clear
 #Menu in the console
 if [ $C = "1" ]
-      then
+then
           echo "Disabling network services"
           (sudo service NetworkManager stop
           sudo ifconfig $( echo $Interface) down
@@ -32,24 +33,18 @@ if [ $C = "1" ]
           )> /dev/null 2>&1
           echo "Your mac adress has been changed:"
           sudo macchanger -r $( echo $Interface)
-      elif [ $C = "2" ]
-        then
-          echo "Disabling network services"
-          (sudo ifconfig $( echo $Interface) down
-          )> /dev/null 2>&1
+elif [ $C = "2" ]
+then
           echo "Starting torghost"
-          (sudo torghost start
-          )> /dev/null 2>&1
-          echo "Rebooting network services"
-          (sudo ifconfig $(echo $Interface) up
-          )> /dev/null 2>&1
+          echo "torghost output:"
+          sudo torghost start
           echo "Your IP adress is now hidden"
-      elif [ $C = "3" ]
-        then
+elif [ $C = "3" ]
+then
             sudo torghost start
 
-      elif [ $C = "4" ]
-        then
+elif [ $C = "4" ]
+then
           clear
           (sudo apt-get install tor
           )> /dev/null 2>&1
@@ -70,6 +65,48 @@ if [ $C = "1" ]
           )> /dev/null 2>&1
           echo ""
           echo "All dependencies installed!"
-        else
+else
           echo "Wrong entry"
 fi
+sleep 5.5
+clear
+#testing if internet still work
+echo "Do  you want to test if your internet connexion still work?"
+echo "1: Yes (recommended)"
+echo "2: No"
+read -p "Your Choice: " N
+clear
+if [ $N = "1" ]
+  then
+Network=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
+              if [ $Network = "ok" ]
+                  then
+                    echo "Internet is working! Nice!"
+              elif [ $Network = "error" ]
+                then
+                    echo "Internet is not working :( Sorry, i try to avoid this bugs in my script, but it's hard...)"
+                    echo ""
+                    echo "I'm sorry, you need to reboot to get internet back..."
+                    echo "Reboot now? (Y/N)"
+                    read -p "Your Choice: " R
+                else
+                  echo "Ups... I have a little problem... Sorry!"
+                fi
+                          if [ $R = "Y" ]
+                          then
+                            reboot
+                          else
+                            echo "Okay, don't forget to reboot later!"
+                          fi
+elif [ $N = "2"]
+then
+  echo "Okay! If you have network issues don't forget to reboot!"
+else
+  echo "I can't understand you..."
+fi
+echo ""
+echo ""
+echo "Thanks for using this script! :) Goodbye!"
+echo ""
+echo ""
+echo ""
