b33e0228666237e98b0844f234505582cac2a74e
Z3r0way <Z3r0way.GitHub@protonmail.com>
2018-04-12 20:10:51 +0200

    replacing torghost for toriptables2, very stable now!

--- Anonymator.bash
+++ Anonymator.bash
@@ -16,49 +16,47 @@ BWHITE='\e[1;37m'
 
 Interface=$(ip route list | grep default | awk '{print $5} ')
 Old_mac=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
-Old_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+Old_IP=$(wget -qO - v4.ifconfig.co)
 
 
 
-#Pause the script when ou need to
+#Pause the script when you need to
 function pause()
 {
   echo -e "${BWHITE}Press [Enter] key to continue...${WHITE}"
    read -p "$*"
 }
-#A fonction to start torghost
-TorGhost ()
-{
-  echo -e "${BWHITE}Starting torghost"
-  echo ""
-  echo "torghost output:"
-  sudo torghost start
-  echo ""
-  echo "Your IP adress is now hidden"
-  echo -e " Remember: If you want to cut torghost, type: ${BRED}sudo torghost stop${WHITE}"
-  echo "(You can also reboot)"
-  pause
-}
 
-#A fonction to change the mac adress with macchanger
-spoof_mac ()
+#A fonction to spoof the mac adress with macchanger
+function spoof_mac ()
 {
           echo -e "${BWHITE}Disabling network services"
           sudo systemctl stop NetworkManager
           echo ""
-          echo "Changing mac adress"
+          echo -e "${BBLUE}Changing mac adress${BWHITE}"
           (sudo macchanger -A $(echo $Interface)
           )> /dev/null 2>&1
           echo ""
           echo "Rebooting network services"
           sudo systemctl start NetworkManager
           echo ""
-          echo -e "Waiting to the network to reboot properly...${WHITE}(15s)"
-          sleep 15s
+          echo -e "Waiting to the network to reboot properly...${WHITE}(can take some time)"
+          sleep 11s
+}
+
+#Reroute all internet trafic throug tor
+function tor_iptables2 ()
+{
+        echo ""
+        echo -e "${BWHITE}Changing iptables rules to ${BBLUE}reroute all trafic throug tor${WHITE}"
+        cd toriptables2
+        (sudo python toriptables2.py -l
+        )> /dev/null 2>&1
+        cd ..
 }
 
 #Test the internet conexion and save it to a variable
-internet_test ()
+function internet_test ()
 {
   Network=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
   if [ $Network = ok ]
@@ -70,7 +68,7 @@ internet_test ()
 }
 
 #Just a litle loop to another fonction to avoid infinite loops
-internet_loop ()
+function internet_loop ()
 {
   if [ $Net_status = "1" ]
   then
@@ -82,13 +80,16 @@ internet_loop ()
 
 
 #Troubleshoot internet issues
-Internet_loss ()
+function Internet_loss ()
 {
   echo -e "${BRED} Oh no! Internet is not working...${BWHITE} I will try to fix the bug and reverting changes...${WHITE}"
                       pause
                       echo ""
-                      sudo torghost stop
-                      sudo ifconfig $(echo $Interface) down
+                      echo ""
+                      cd toriptables2
+                      (sudo python toriptables2.py -f
+                      )> /dev/null 2>&1
+                      cd ..                      sudo ifconfig $(echo $Interface) down
                       sudo service network-manager stop
                       sudo macchanger -p $(echo $Interface)
                       sudo service network-manager start
@@ -121,10 +122,10 @@ Internet_loss ()
 
 
 #A fonction to show your ip adress and mac adress
-informations ()
+function informations ()
 {
   New_mac=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
-  New_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+  New_IP=$(wget -qO - v4.ifconfig.co)
   clear
   echo ""
   echo -e "${BWHITE}You are now anonymous! :)"
@@ -136,15 +137,39 @@ informations ()
   echo $Old_mac
   echo ""
   echo -e "${BGREEN}ACTUAL ip adress: ${BBLUE}"
-  echo $New_IP
+  echo -e "$New_IP|Check it here!: ${BWHITE}https://check.torproject.org/${WHITE} (ctrl + click)"
   echo -e "${BGREEN}OLD ip adress: ${BBLUE}"
   echo $Old_IP
-  echo -e "Check it here!: ${BWHITE}https://duckduckgo.com/ip${WHITE} (ctrl + click)"
   echo ""
-  echo -e "${WHITE}"
-  pause
+  echo -e "${BWHITE}Press [Enter] when you want to ${BRED}stop${BWHITE} the script and revert changes"
+  read -p "$*"
 }
 
+#A fonction to revert changes
+function STOP ()
+{
+
+  clear
+  echo ""
+  echo -e "${BWHITE}Changing iptables rules to ${BBLUE}stop rerouting all trafic throug tor${WHITE}"
+  cd toriptables2
+  (sudo python toriptables2.py -f
+  )> /dev/null 2>&1
+  cd ..
+  echo ""
+  echo -e "${BWHITE}Disabling network services"
+  sudo systemctl stop NetworkManager
+  echo ""
+  echo -e "${BBLUE}Reverting mac adress to the permanent one${BWHITE}"
+  (sudo macchanger -p $(echo $Interface)
+  )> /dev/null 2>&1
+  echo ""
+  echo "Rebooting network services"
+  sudo systemctl start NetworkManager
+  echo ""
+  echo -e "Waiting to the network to reboot properly...${WHITE}(can take some time)"
+  sleep 11s
+}
 
 
 
@@ -154,10 +179,10 @@ echo          "|  _  |___ ___ ___ _ _ _____ ___| |_ ___ ___  "
 echo          "|     |   | . |   | | |     | .'|  _| . |  _| "
 echo          "|__|__|_|_|___|_|_|_  |_|_|_|__,|_| |___|_|   "
 echo -e       "                  |___|                       "
-echo -e "                    ${BBLUE}Made with <3 From France${BWHITE}"
+echo -e "       ${BBLUE}Made with <3 From France by Timothe P${BWHITE}"
 echo -e "1: Spoof your mac adress"
-echo "2: Reroute all your conexions throug tor (Unstable)"
-echo "3: Do both (Unstable)"
+echo "2: Reroute all your conexions throug tor"
+echo -e "3: Do both (${BGREEN}Recomanded${BWHITE})"
 echo -e "4: Install dependencies (you need to do it once)${BBLUE}"
 
 
@@ -174,23 +199,26 @@ then
           internet_test
           internet_loop
           informations
+          STOP
 
 
 elif [ $C = "2" ]
 then
-          TorGhost
+          tor_iptables2
           internet_test
           internet_loop
           informations
+          STOP
 
 
 elif [ $C = "3" ]
 then
-            spoof_mac
-            TorGhost
-            internet_test
-            internet_loop
-            informations
+          spoof_mac
+          tor_iptables2
+          internet_test
+          internet_loop
+          informations
+          STOP
 
 
 elif [ $C = "4" ]
@@ -200,9 +228,9 @@ then
           (sudo apt-get install tor -y
           )> /dev/null 2>&1
           echo ""
-          echo "Downloading and installing macchanger"
+          echo -e "Downloading and installing macchanger${WHITE}"
           sudo apt-get install macchanger -y
-          echo ""
+          echo -e "${BWHITE}"
           echo "Downloading and installing python pip"
           echo "(+/- 28MB + dependencies) -> Can take some time"
           (sudo apt-get install python-pip -y
@@ -213,18 +241,8 @@ then
           (sudo pip2 install steam
           )> /dev/null 2>&1
           echo ""
-          echo "Downloading Torghost"
-          (git clone https://github.com/susmithHCK/torghost.git
+          (sudo rm -rf torghost
           )> /dev/null 2>&1
-          echo ""
-          echo "Installing Torghost"
-          (cd torghost
-          chmod +x install.sh
-          sudo ./install.sh
-          cd ..
-          sudo rm -rf torghost
-          )> /dev/null 2>&1
-          echo ""
           echo "Installing net-tools"
           echo ""
           (sudo apt install net-tools
@@ -233,9 +251,13 @@ then
           echo ""
           (sudo apt-get install network-manager
           )> /dev/null 2>&1
+          echo "Downloading Toriptables 2"
+          echo ""
+          (git clone https://github.com/ruped24/toriptables2
+          )> /dev/null 2>&1
           echo -e "${BGREEN}All dependencies installed!"
           echo ""
-          echo "Restart the script to use it! :)"
+          echo "Restart the script to use it! "
 else
-          echo -e "${BRED}Wrong entry${BWHITE}"
+echo -e "${BRED}Wrong entry${BWHITE}"
 fi
